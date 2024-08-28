a463763d888de668dc867e4d4ee6ef25c3319ad0
mayaning <mayaning4coding@163.com>
2024-08-28 16:56:10 +0800

    初始化

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,13 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "c", "cpp", "md", "txt", "c.snippets", "cpp.snippets" },
+  callback = function()
+    vim.b.autoformat = false -- 取消C语言的autoformat
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+-- local unmap = vim.keymap.del
+
+-- Ctrl+j 向下跳10行
+map({ "n", "v" }, "<C-j>", "10j")
+-- Ctrl+k 向上跳10行
+map({ "n", "v" }, "<C-k>", "10k")
