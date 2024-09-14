03676e5f80ba1344402381ec52ce24ebf3534048
mayaning <mayaning4coding@163.com>
2024-09-14 17:46:26 +0800

    取消bash脚本的自动format

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c", "cpp", "md", "txt", "c.snippets", "cpp.snippets" },
+  pattern = { "c", "cpp", "md", "txt", "c.snippets", "cpp.snippets", "sh" },
   callback = function()
     vim.b.autoformat = false -- 取消C语言的autoformat
     vim.opt_local.expandtab = true
